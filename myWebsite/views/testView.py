96317691283b341ae97f2858f96836fbad860cdc
ZeroWhiteSmile <x2335883503@163.com>
2020-04-26 20:15:29 +0800

    接口修改

--- myWebsite/views/testView.py
+++ myWebsite/views/testView.py
@@ -10,32 +10,42 @@ from myWebsite.models.testModel import *   # 导入数据模块
 
 @require_http_methods(['GET'])
 def add_book(request):
-    response = {}
     try:
         book = Book(book_name=request.GET.get('book_name'))
         book.save()
-        response['msg'] = 'success'
-        response['error_num'] = 0
+        response = {
+            'msg': 'success',
+            'code': 200,
+        }
     except Exception as e:
         response['msg'] = str(e)
-        response['error_num'] = 1
-
+        response['code'] = 500
     return JsonResponse(response)
 
 @require_http_methods(["GET"])
 def show_books(request):
-    response = {}
     try:
         books = Book.objects.filter()
-        response['list'] = json.loads(serializers.serialize("json", books))
-        response['msg'] = 'success'
-        response['error_num'] = 0
-    except  Exception as e:
+        response = {
+            'list': json.loads(serializers.serialize("json", books)),
+            'msg': 'success',
+            'code': 200
+        }
+    except Exception as e:
         response['msg'] = str(e)
-        response['error_num'] = 1
+        response['code'] = 500
     return JsonResponse(response)
 
 
 @require_http_methods(["GET"])
 def msg(request, name, age):
-    return HttpResponse('My name is ' + name + ',i am ' + age + ' years old')
+    try:
+        response = {
+            'list': 'My name is ' + name + ',i am ' + age + ' years old',
+            'msg': 'success',
+            'code': 200
+        }
+    except Exception as e:
+        response['msg'] = str(e)
+        response['code'] = 500
+    return JsonResponse(response)
--- web_python3/urls.py
+++ web_python3/urls.py
@@ -29,7 +29,7 @@ import myWebsite.urls
 # 此处路由地址都是在server地址 http://127.0.0.1:8000/后的路径
 urlpatterns = [
     path('admin/', admin.site.urls),
-    url(r'^api/test/', include(myWebsite.urls)), # 函数 include() 允许引用其它 URLconfs。每当 Django 遇到 include() 时，它会截断与此项匹配的 URL 的部分，并将剩余的字符串发送到 URLconf 以供进一步处理。
+    url(r'^api/test/v1/', include(myWebsite.urls)), # 函数 include() 允许引用其它 URLconfs。每当 Django 遇到 include() 时，它会截断与此项匹配的 URL 的部分，并将剩余的字符串发送到 URLconf 以供进一步处理。
     url(r'^$', TemplateView.as_view(template_name="index.html")),
     url(r'^develop/(?P<path>.*)$', TemplateView.as_view(template_name="index.html")),  # 刷新后页面404问题，有多少个路由就添加多少个url，好烦。
     url(r'^test/(?P<path>.*)$', TemplateView.as_view(template_name="index.html")),  # 刷新后页面404问题，有多少个路由就添加多少个url，好烦。
